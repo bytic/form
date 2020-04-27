341dc17dc1da7626ee696e23b05ed35a6cdeab79
Gabriel Solomon <hello@gabrielsolomon.ro>
2020-04-27 10:53:44 +0300

    update form model

---
+++ legacy/Model.php
@@ -0,0 +1,11 @@
+<?php
+
+use Nip\Form\FormModel;
+
+/**
+ * Class Nip_Form_Model
+ * @deprecated use \Nip\Form\FormModel
+ */
+class Nip_Form_Model extends FormModel
+{
+}
---
+++ src/FormModel.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nip\Form;
+
+/**
+ * Class FormModel
+ * @package Nip\Form
+ */
+class FormModel extends Form
+{
+    use \Nip\Form\Traits\HasModelTrait;
+}
