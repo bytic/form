aa2eaa7dd31366af002fae866b883cff55f6e2cc
Gabriel Solomon <hello@gabrielsolomon.ro>
2026-03-29 21:06:37 +0300

    feat: add time input type to Nip_Form_Element_Time and add laravel-idea.xml config

--- src/Renderer/Elements/Time.php
+++ src/Renderer/Elements/Time.php
@@ -2,4 +2,17 @@
 
 class Nip_Form_Renderer_Elements_Time extends Nip_Form_Renderer_Elements_Input
 {
+
+    /**
+     * @return array
+     */
+    public function getElementAttribs()
+    {
+        $attribs = parent::getElementAttribs();
+        $attribs[] = 'step';
+        $attribs[] = 'min';
+        $attribs[] = 'max';
+
+        return $attribs;
+    }
 }
